1603db9718b23c8dc2d3744a5162f2cd1d79b3ce
Rido <rido-min@users.noreply.github.com>
2025-03-21 07:51:46 +0000

    Refactor activity object IDs and enhance logging for clarity

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import { Activity, ActivityTypes } from '@microsoft/agents-bot-activity';
 
 const act = Activity.fromObject({
   type: ActivityTypes.Message,
-  id: 'a123',
+  id: 'a1',
   channelId: 'default-channel',
   serviceUrl: 'https://example.com',
   conversation: {
-    id: 'c2',
+    id: 'c1',
   },
   recipient: {
     id: 'bot1',
@@ -14,16 +14,16 @@ const act = Activity.fromObject({
   },
 });
 
-const act2 = Activity.fromObject(act);
 console.log(act);
 console.log(act.id);
 console.log(act.conversation.id);
 console.log(act.getConversationReference());
-
+console.log(JSON.stringify(act))
 console.log('\n--act2--\n');
-act2.id = '456';
-delete act2.conversation;
+const act2 = Activity.fromObject(act);
+act2.id = 'a2';
+act2.conversation.id  = 'c2'
 console.log(act2);
 console.log(act2.id);
 console.log(act2.conversation?.id);
-console.log(act.getConversationReference());
+console.log(act2.getConversationReference());
